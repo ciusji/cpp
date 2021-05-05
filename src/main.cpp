48696151eb8a49cb708bb13c39137b0e14f0e426
ciusji <bqjimaster@gmail.com>
2021-05-05 22:46:23 +0800

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "sqlite3.h"
+
+using namespace std;
+
+sqlite3 * pDB = NULL;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    int nRes = sqlite3_open("~/test.db", &pDB);
+    if (nRes != SQLITE_OK) {
+        cout<<"Open Sqlite3 Failed"<<sqlite3_errmsg(pDB);
+    }
     return 0;
 }
